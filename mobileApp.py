5001a596c32a90b03e68d0574b6f109f027f10af
olododo omotomiwa <omotomiwar@gmail.com>
2023-12-19 20:56:02 +0100

    changed the file type to .kv

--- mobileApp.py
+++ mobileApp.py
@@ -1,37 +1,34 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
-from ScreenSaver import screen_helper
-from kivy.uix.filechooser import FileChooser
 
 
 
+class CreatePlaylistScreen(Screen):
+    pass
 
 class HomeScreen(Screen):
     pass
 
-
 class AddScreen(Screen):
     pass
 
-
 class PlaylistScreen(Screen):
      pass
-
 
 Sm = ScreenManager()
 Sm.add_widget(HomeScreen(name='Home'))
 Sm.add_widget(AddScreen(name='add'))
 Sm.add_widget(PlaylistScreen(name='playlist'))
+Sm.add_widget(CreatePlaylistScreen(name='createPlaylist'))
 
 
 class M_A_S_T(MDApp):
     def build(self):
-        screen = Builder.load_string(screen_helper)
+        screen = Builder.load_file('ScreenSaver.kv')
         return screen
     
     def getaudio(self):
-        file  = FileChooser(filters=['*.mp3', '*.wav'])
         print("successful")
 
     def on_tab_press(self, tab_name):
