ae7080c8d976063cf266d93438ecd97a146ab101
olododo omotomiwa <omotomiwar@gmail.com>
2023-12-24 09:06:00 +0100

    created the audio player screen still incomplete, adding firebase realtime database to the application

--- mobileApp.py
+++ mobileApp.py
@@ -2,9 +2,20 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition, NoTransition
 from kivy.core.window import Window
+from dotenv import load_dotenv
+
+
 Window.size = (320, 600)
 
+
+load_dotenv()
+
+
+
 class CreatePlaylistScreen(Screen):
+    pass
+
+class AudioPlayer(Screen):
     pass
 
 class HomeScreen(Screen):
@@ -21,6 +32,7 @@ Sm.add_widget(HomeScreen(name='Home'))
 Sm.add_widget(AddScreen(name='add'))
 Sm.add_widget(PlaylistScreen(name='playlist'))
 Sm.add_widget(CreatePlaylistScreen(name='createPlaylist'))
+Sm.add_widget(AudioPlayer(name='audio_player'))
 
 
 
@@ -29,16 +41,42 @@ class M_A_S_T(MDApp):
         screen = Builder.load_file('ScreenSaver.kv')
         return screen
     
-    def getaudio(self):
+    def addSong(self):
         print("successful")
 
-    def on_tab_press(self, tab_name):
-        if tab_name == 'home':
-            self.root.current = 'Home'
-        elif tab_name == 'addSong':
-            self.root.current = 'add'
-        elif tab_name == 'playlist':
-            self.root.current = 'playlist'
+    def createplaylistfu(self, choose):
+        if choose == 'typed_parm':
+            playlistname1 = self.root.get_screen('createPlaylist').ids.playlist_names.text
+            rhythm = self.root.get_screen('createPlaylist').ids.parm_one.text
+            pitch = self.root.get_screen('createPlaylist').ids.parm_two.text
+            tempo = self.root.get_screen('createPlaylist').ids.parm_three.text
+
+        elif choose == 'upload':
+            playlistname2 = self.root.get_screen('createPlaylist').ids.playlistnames.text
+
+
+       
+
+
+    def auidoPlayerS(self, tab_name):
+        if tab_name == 'happy':
+            self.root.current = 'audio_player'
+            self.root.get_screen('audio_player').ids.playlistname.text = 'Happy playlist'
+            self.root.get_screen('audio_player').ids.playlistImage.source = 'static/happy.png'
+        elif tab_name == 'sad':
+            self.root.current = 'audio_player'
+            self.root.get_screen('audio_player').ids.playlistname.text = 'Sad playlist'
+            self.root.get_screen('audio_player').ids.playlistImage.source = 'static/sadnigga.png'
+        elif tab_name == 'chill':
+            self.root.current = 'audio_player'
+            self.root.get_screen('audio_player').ids.playlistname.text = 'Chill playlist'
+            self.root.get_screen('audio_player').ids.playlistImage.source = 'static/chill_guy.jpg'
+
+
+    def searchSong(self, search):
+        search = self.root.get_screen('playlist').ids.Searchs.text
+        if search:
+            pass
 
 
 
